6cc1de1cd9dd00c44e955085da21f7572dc78cdf
patmanf <27874300+patmanf@users.noreply.github.com>
2021-09-28 14:21:00 -0500

    use generic script context instead of companions

--- pat/snusk/snusk.lua
+++ pat/snusk/snusk.lua
@@ -1,19 +1,10 @@
-local snusk_init = init
-local snusk_update = update
-
-function init()
-	snusk_init()
-	
-	self.snuskTimer = math.random(120, 300)
-end
+local snuskTimer = math.random(120, 300)
 
 function update(dt)
-	snusk_update(dt)
+	snuskTimer = math.max(0, snuskTimer - dt)
 	
-	self.snuskTimer = math.max(0, self.snuskTimer - dt)
-	
-	if self.snuskTimer == 0 then
-		self.snuskTimer = math.random(240, 600)
+	if snuskTimer == 0 then
+		snuskTimer = math.random(240, 600)
 		if math.random(1, 1000) == 1 then
 			player.radioMessage("pat_snusk")
 		end
